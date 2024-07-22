3303cb92f71c20e11686771287058b4e44e2fd14
gabbwhxz <vnvsgabe@gmail.com>
2024-07-22 18:21:52 -0300

    criado interface de vento

--- api/src/event/interfaces/event.interface.ts
+++ api/src/event/interfaces/event.interface.ts
@@ -1,7 +1,11 @@
+// ? define a forma de um objeto TypeScript, sem impor validações
+// ? sao uteis para descrever a estrutura dos dados que a aplicaçao manipula internamente.
+
 export interface Event {
   id: string;
   title: string;
   start: Date;
   end: Date;
+  backgroundColor: string;
   resourceId: string;
 }
