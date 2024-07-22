363bd9c33d5a4d21408edb94d0ec9e6fc9dbc819
gabbwhxz <vnvsgabe@gmail.com>
2024-07-22 18:22:12 -0300

    criado metodo post para o event controller

--- api/src/event/event.controller.ts
+++ api/src/event/event.controller.ts
@@ -1,9 +1,34 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { v4 as uuidv4 } from 'uuid';
+
+import { CreateEventDto } from './dtos/createEvent.dto';
+import { Event } from './interfaces/event.interface';
 
 @Controller('event')
 export class EventController {
+  private events: Event[] = [];
+
   @Post()
-  async createEvent() {
-    return JSON.stringify({ test: 'hello world' });
+  create(@Body() createEventDto: CreateEventDto): {
+    message: string;
+    event: Event;
+  } {
+    const newEvent: Event = {
+      id: uuidv4(),
+      title: createEventDto.title,
+      start: createEventDto.start,
+      end: createEventDto.end,
+      backgroundColor: '#64BAB8',
+      resourceId: createEventDto.resourceId,
+    };
+
+    this.events.push(newEvent);
+    console.log(this.events);
+
+    return {
+      message: `Evento criado com sucesso: ${newEvent.title}`,
+      event: newEvent,
+    };
   }
 }
